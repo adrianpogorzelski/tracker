6d6b7151263bac6f6e0671cd4268736c4ecaa872
Adrian <pogorzelski.adrian@gmail.com>
2023-05-07 18:49:35 +0200

    Initial commit

---
+++ src/test/java/Tracker/TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
