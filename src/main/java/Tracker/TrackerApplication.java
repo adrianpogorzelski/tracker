8d3cd924dc7bbfe4b331cff00188daea09e81651
Adrian <pogorzelski.adrian@gmail.com>
2023-05-15 13:21:13 +0200

    Added Spring Security

--- src/main/java/Tracker/TrackerApplication.java
+++ src/main/java/Tracker/TrackerApplication.java
@@ -2,8 +2,10 @@ package Tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity(securedEnabled = true)
 public class TrackerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/Tracker/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package Tracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/", "/index").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin((form) -> form
+                .loginPage("/login").permitAll()
+            )
+            .logout((logout) -> logout.permitAll());
+        return httpSecurity.build();
+    }
+}
