a1f241871a98aeecb60d81584fd7b859e76c2b87
Adrian <pogorzelski.adrian@gmail.com>
2023-05-18 16:33:22 +0200

    Added new project view Updated navigation to point to new view Can add project Added basic validation

--- src/main/java/Tracker/project/ProjectController.java
+++ src/main/java/Tracker/project/ProjectController.java
@@ -1,12 +1,16 @@
 package Tracker.project;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -21,6 +25,29 @@ public class ProjectController {
         ModelAndView modelAndView = new ModelAndView("/projects/index");
         List<Project> projects = projectService.findAll();
         modelAndView.addObject("projects", projects);
+        return modelAndView;
+    }
+
+    @GetMapping("/new")
+    ModelAndView newProject() {
+        ModelAndView modelAndView = new ModelAndView("/projects/new");
+        Project project = new Project();
+        modelAndView.addObject("project", project);
+        return modelAndView;
+    }
+
+    @PostMapping("/save")
+    ModelAndView save(@ModelAttribute @Valid Project project, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView("redirect:/projects");
+
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("projects/new");
+            modelAndView.addObject("project", project);
+            return modelAndView;
+        }
+
+        project.setDateCreated(LocalDateTime.now());
+        projectService.save(project);
         return modelAndView;
     }
 }
--- src/test/java/Tracker/project/ProjectControllerTest.java
+++ src/test/java/Tracker/project/ProjectControllerTest.java
@@ -1,5 +1,48 @@
+/* 
+CAN'T GET TESTS TO WORK
+
+
 package Tracker.project;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+
+import jakarta.transaction.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
 public class ProjectControllerTest {
+
+    private ProjectRepository projectRepository;
     
+    @Autowired
+    private ProjectController projectController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(projectController)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    
+    @Test
+    @WithMockUser(roles = "USER")
+    public void cantAddProjectWithEmptyName() throws Exception {
+        Project project = new Project();
+        mockMvc.perform(post("/projects/save")
+            .flashAttr("project", project))
+            .andExpect(status().isBadRequest());
+    }
 }
+ */
