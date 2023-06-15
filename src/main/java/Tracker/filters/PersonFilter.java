830112605885cd8994120047a354d440c4739189
Adrian <pogorzelski.adrian@gmail.com>
2023-06-15 14:56:14 +0200

    Filtering works with null values (all values)

---
+++ src/main/java/Tracker/filters/PersonFilter.java
@@ -0,0 +1,12 @@
+package Tracker.filters;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class PersonFilter {
+    private Long id;
+}
